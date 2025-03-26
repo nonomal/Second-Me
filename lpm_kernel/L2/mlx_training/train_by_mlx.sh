b5e6632146e8e396f6489bff81aec1547b274be2
yingapple <yingxiang835@gmail.com>
2025-03-26 15:39:38 +0800

    feat(mlx_training_support): init training pipeline with support for training, merging, serving, and testing

---
+++ lpm_kernel/L2/mlx_training/train_by_mlx.sh
@@ -0,0 +1,13 @@
+mlx_lm.lora --model mlx-community/Qwen2.5-7B-Instruct-4bit \
+--train \
+--batch-size 2 \
+--num-layers 28 \
+--max_seq_length 2048 \
+--learning-rate 1e-5 \
+--iters 200 \
+--steps_per_report 10 \
+--steps_per_eval 10 \
+--save_every 100 \
+--lora_parameters "{\"rank\": 32, \"alpha\": 64, \"dropout\": 0.1, \"scale\": 10.0}" \
+--adapter_path "resources/model/output/mlx/adapters" \
+--data "resources/data/mlx_train_data" \
