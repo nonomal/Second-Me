b096b96b81a41816854fa392e89710dbcd3841f0
Xiang Ying <yingxiang835@gmail.com>
2025-03-30 17:53:57 +0800

    Revert "Mlx training support (#109)"
    This reverts commit e705b4faeb6acee1d8b6a28b3ce459a694264b7d.

--- lpm_kernel/L2/mlx_training/train_by_mlx.sh
+++ lpm_kernel/L2/mlx_training/train_by_mlx.sh
@@ -1,16 +1,13 @@
-# start the training process in terminal(can not set the lora params)
-# mlx_lm.lora --model mlx-community/Qwen2.5-7B-Instruct-4bit \
-# --train \
-# --batch-size 2 \
-# --num-layers 28 \
-# --max_seq_length 2048 \
-# --learning-rate 1e-5 \
-# --iters 200 \
-# --steps_per_report 10 \
-# --steps_per_eval 10 \
-# --save_every 100 \
-# --adapter_path "resources/model/output/mlx/adapters" \
-# --data "resources/data/mlx_train_data" \
-
-# start the training process and set the config in .yaml file(can set the lora params and other params)
-mlx_lm.lora -c lora_config.yaml
+mlx_lm.lora --model mlx-community/Qwen2.5-7B-Instruct-4bit \
+--train \
+--batch-size 2 \
+--num-layers 28 \
+--max_seq_length 2048 \
+--learning-rate 1e-5 \
+--iters 200 \
+--steps_per_report 10 \
+--steps_per_eval 10 \
+--save_every 100 \
+--lora_parameters "{\"rank\": 32, \"alpha\": 64, \"dropout\": 0.1, \"scale\": 10.0}" \
+--adapter_path "resources/model/output/mlx/adapters" \
+--data "resources/data/mlx_train_data" \
